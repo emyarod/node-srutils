0ed91aee34260440347eb2eda9abd6ac2aa5b985
emyarod <afw5059@gmail.com>
2018-03-15 22:48:13 -0400

    Add newlines

--- src/commands/config.js
+++ src/commands/config.js
@@ -12,16 +12,16 @@ export default async function config() {
   const clientId = await getCredential({
     rl,
     prompt:
-      'Enter your reddit client ID (follow these steps to obtain it https://github.com/reddit-archive/reddit/wiki/OAuth2-Quick-Start-Example#first-steps): ',
+      '\nEnter your reddit client ID (follow these steps to obtain it https://github.com/reddit-archive/reddit/wiki/OAuth2-Quick-Start-Example#first-steps): ',
   });
   const clientSecret = await getCredential({
     rl,
     prompt:
-      'Enter your reddit client secret (follow these steps to obtain it https://github.com/reddit-archive/reddit/wiki/OAuth2-Quick-Start-Example#first-steps): ',
+      '\nEnter your reddit client secret (follow these steps to obtain it https://github.com/reddit-archive/reddit/wiki/OAuth2-Quick-Start-Example#first-steps): ',
   });
   const refreshToken = await getCredential({
     rl,
-    prompt: `Enter your reddit refresh token (you can generate one here https://not-an-aardvark.github.io/reddit-oauth-helper/). Make sure your token contains the following scopes (flair, modconfig, modflair, mysubreddits, read, structuredstyles). Or press 'Enter' to skip this step: `,
+    prompt: `\nEnter your reddit refresh token (you can generate one here https://not-an-aardvark.github.io/reddit-oauth-helper/). Make sure your token contains the following scopes (flair, modconfig, modflair, mysubreddits, read, structuredstyles). Or press 'Enter' to skip this step: `,
   });
   rl.close();
   fs.writeFileSync(
